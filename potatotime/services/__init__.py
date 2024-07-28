878ffae2f03f5e97c9d373bd5d94b5028dff968e
Alvin Wan <alvinwan1@gmail.com>
2024-07-27 20:50:45 -0700

    added interface

---
+++ potatotime/services/__init__.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class CalendarServiceInterface(ABC):
+    @abstractmethod
+    def authorize(self):
+        pass
+
+    @abstractmethod
+    def get_event(self, event_id):
+        pass
+
+    @abstractmethod
+    def get_events(self):
+        pass
+
+    @abstractmethod
+    def create_event(self, event_data):
+        pass
+
+    @abstractmethod
+    def update_event(self, event_id, update_data):
+        pass
+
+    @abstractmethod
+    def delete_event(self, event_id):
+        pass
