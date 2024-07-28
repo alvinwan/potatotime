7d549f1c57bcdcf6089443aefe892b3c43576830
Alvin Wan <alvinwan1@gmail.com>
2024-07-28 05:23:46 -0700

    do not sync already-sycned events

--- potatotime/synchronize.py
+++ potatotime/synchronize.py
@@ -24,16 +24,22 @@ def synchronize_from_to(
     calendar2: CalendarServiceInterface,
     events2: List[CalendarEvent]
 ) -> List[str]:
+    source_event_ids = {event.source_event_id for event in events2}
+
     new_events = []
     for event1 in events1:
         # Do not copy any events that were created by potatotime
-        if event1.is_copy:
+        if event1.source_event_id is not None:
             continue
-        
+
+        # Do not copy any events that have already been copied
+        if event1.id in source_event_ids:
+            continue
+
         # TODO: distinguish between stubs and fully materialized events with ids
         event2_data = calendar2.CalendarEvent.from_(event1).serialize()
         event2_data.pop('id')  # stub would not have had this id
-        event2_data = calendar2.create_event(event2_data)
+        event2_data = calendar2.create_event(event2_data, source_event_id=event1.id)
         event2 = calendar2.CalendarEvent.deserialize(event2_data)
         new_events.append(event2)
     return new_events
